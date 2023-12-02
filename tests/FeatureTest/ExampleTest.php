4a1504adc7060e5f470e2dff1a514acd4006ff6d
Sumeet Ghimire <42274174+sumeetghimire@users.noreply.github.com>
2023-12-02 14:50:28 +0530

    Add files via upload

---
+++ tests/FeatureTest/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use SumeetGhimire\EncryptDecryptWithKey\EncryptionHelper;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        // Encrypt a string
+        $originalString = 'Hello, Laravel!';
+        $encryptedString = EncryptionHelper::encryptString($originalString);
+
+        // Decrypt the string
+        $decryptedString = EncryptionHelper::decryptString($encryptedString);
+
+        // Assert that the decrypted string matches the original string
+        $this->assertEquals($originalString, $decryptedString);
+    }
+}
